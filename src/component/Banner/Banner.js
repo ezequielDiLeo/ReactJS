7a62be2092b1cc3050f896e04b67b6f9bb794839
Ezequiel Di Leo <ezemr1996@gmail.com>
2023-09-13 11:03:05 -0300

    react

---
+++ src/component/Banner/Banner.js
@@ -0,0 +1,12 @@
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="banner-container">
+                <h1>e-commerce</h1>
+                <p>The Ezequiel*s e-commerce to find your result</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/component/Home/Home.js
@@ -0,0 +1,16 @@
+import Navbar from "../Navbar/Navbar";
+import Banner from "../Banner/Banner";
+import About from "../about/About";
+
+
+
+const Home = () => {
+    return(
+    <>
+        <Navbar />
+        <About />
+        <Banner />
+    </>);
+};
+
+export default Home;
